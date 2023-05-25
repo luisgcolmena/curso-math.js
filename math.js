091bdf3517c08081f2b5c0fc0a13734e6ffabbf2
Luis Gerardo <luisgcolmena@gmail.com>
2023-05-24 20:49:31 -0400

    Agregando validación a los inputs

--- math.js
+++ math.js
@@ -1,12 +1,24 @@
 const btn = document.querySelector("#calcular")
-const price = document.querySelector("#price")
-const discount = document.querySelector("#discount")
+const inputPrice = document.querySelector("#price")
+const inputDiscount = document.querySelector("#discount")
 const result = document.querySelector("#new-price")
 
 btn.addEventListener('click', calcularDescuento)
 
+console.log({inputDiscount})
+
 function calcularDescuento () {
-    const newPrice = price * (discount/100)
-    const productPrice = document.createElement("p")
-    productPrice.innerHTML = newPrice
+    const price = inputPrice.value
+    const discount = inputDiscount.value
+
+    if (!price || !discount) {
+        result.innerText = 'Precio o descuento inválido.'
+    } else if (discount > 100) {
+        result.innerText = 'Descuentos mayores a 100 son inválidos.'
+    } else {    
+        const newPrice = price * (1 - (discount/100)) 
+        console.log(newPrice)
+        result.innerText = 'El precio con descuento es: ' + newPrice
+    }
+
 }
