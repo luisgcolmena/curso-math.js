16e2f4c4fad03bff854cad7a297eebf3af44d36f
Luis Gerardo <luisgcolmena@gmail.com>
2023-05-27 16:18:15 -0400

    Cambiando el feature de Descuento por Cupón de Descuento

--- math.js
+++ math.js
@@ -1,11 +1,9 @@
 const btn = document.querySelector("#calcular")
 const inputPrice = document.querySelector("#price")
-const inputDiscount = document.querySelector("#discount")
+const inputCoupon = document.querySelector("#discount-coupon")
 const result = document.querySelector("#new-price")
 
-btn.addEventListener('click', calcularDescuento)
-
-console.log({inputDiscount})
+btn.addEventListener('click', calcularCuponDescuento)
 
 function calcularDescuento () {
     const price = inputPrice.value
@@ -19,6 +17,29 @@ function calcularDescuento () {
         const newPrice = price * (1 - (discount/100)) 
         console.log(newPrice)
         result.innerText = 'El precio con descuento es: ' + newPrice
+    }
+
+}
+
+function calcularCuponDescuento () {
+    const price = inputPrice.value
+    const coupon = inputCoupon.value
+
+    const couponList = {
+        "SOMBRI30": 30, 
+        "SOMBRI50": 50, 
+        "SOMBRI75": 75
+    }
+
+    if (!price || !coupon) {
+        result.innerText = 'Precio o descuento inválido.'
+    } else if (couponList[coupon]) {
+        console.log("Boop")
+        const discount = couponList[coupon]
+        const newPrice = price * (1 - (discount/100))
+        result.innerText = 'El precio con descuento es: ' + newPrice
+    } else {    
+        result.innerText = 'Cupón de descuento inválido.'
     }
 
 }
