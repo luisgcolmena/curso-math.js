48cc586630e203434a8372c1dc911185f5f5082d
Luis Gerardo <luisgcolmena@gmail.com>
2023-05-27 21:13:40 -0400

    Añadiendo la funcion que selecciona el cupon correcto usando métodos de manipulación de arrays

--- math.js
+++ math.js
@@ -3,7 +3,7 @@ const inputPrice = document.querySelector("#price")
 const inputCoupon = document.querySelector("#discount-coupon")
 const result = document.querySelector("#new-price")
 
-btn.addEventListener('click', calcularCuponDescuento)
+btn.addEventListener('click', calcularCuponDescuentoArray)
 
 function calcularDescuento () {
     const price = inputPrice.value
@@ -36,6 +36,59 @@ function calcularCuponDescuento () {
     } else if (couponList[coupon]) {
         console.log("Boop")
         const discount = couponList[coupon]
+        const newPrice = price * (1 - (discount/100))
+        result.innerText = 'El precio con descuento es: ' + newPrice
+    } else {    
+        result.innerText = 'Cupón de descuento inválido.'
+    }
+
+}
+
+function calcularCuponDescuentoArray () {
+    const price = inputPrice.value
+    const coupon = inputCoupon.value
+
+    const couponList = [{
+        name: "SOMBRI15", 
+        number: 15, 
+        isValid: false
+        },
+        {
+            name: "SOMBRI30", 
+            number: 30, 
+            isValid: true
+        },
+        {
+            name: "SOMBRI50", 
+            number: 50, 
+            isValid: true
+        },
+        {
+            name: "SOMBRI75", 
+            number: 75, 
+            isValid: true
+        },
+        {
+            name: "SOMBRI90", 
+            number: 90, 
+            isValid: true
+        }
+    ]
+
+    const validCoupon = couponList.find(
+        function isDiscountValid (element) {
+            if (element.name == coupon) {
+                return true
+            }
+        }
+    )
+
+
+    if (!price || !coupon) {
+        result.innerText = 'Precio o descuento inválido.'
+    } else if (validCoupon) {
+        console.log("Boop")
+        const discount = validCoupon.number
         const newPrice = price * (1 - (discount/100))
         result.innerText = 'El precio con descuento es: ' + newPrice
     } else {    
